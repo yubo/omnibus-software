b2dea3583de18482dbed60fbf1b691339c35ce3f
Ho-Sheng Hsiao <hosh@getchef.com>
2014-03-06 13:17:59 -0700

    Backport ipv6-enabled Nginx 1.4.4 from master

--- config/software/nginx.rb
+++ config/software/nginx.rb
@@ -1,13 +1,13 @@
 #
-# Copyright:: Copyright (c) 2012 Opscode, Inc.
+# Copyright:: Copyright (c) 2012-2014 Chef Software, Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,19 +16,21 @@
 #
 
 name "nginx"
-version "1.2.3"
+default_version "1.4.4"
 
-dependencies ["pcre"]
+dependency "pcre"
 
-source :url => "http://nginx.org/download/nginx-1.2.3.tar.gz",
-       :md5 => "0a986e60826d9e3b453dbefc36bf8f6c"
+source :url => "http://nginx.org/download/nginx-#{version}.tar.gz",
+       :md5 => "5dfaba1cbeae9087f3949860a02caa9f"
 
-relative_path "nginx-1.2.3"
+relative_path "nginx-#{version}"
 
 build do
   command ["./configure",
            "--prefix=#{install_dir}/embedded",
            "--with-http_ssl_module",
+           "--with-http_stub_status_module",
+           "--with-ipv6",
            "--with-debug",
            "--with-ld-opt=-L#{install_dir}/embedded/lib",
            "--with-cc-opt=\"-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include\""].join(" ")
