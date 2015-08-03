c3feee0d44712fa559c7fe00affba4bdf42b5d28
Etienne LAFARGE <etienne.lafarge@datadoghq.com>
2015-08-03 10:39:28 -0400

    To rebase: rrd tool perm fix

--- config/software/python-rrdtool.rb
+++ config/software/python-rrdtool.rb
@@ -4,7 +4,7 @@ dependency "python"
 
 build do
   ship_license "https://raw.githubusercontent.com/oetiker/rrdtool-1.x/master/COPYRIGHT"
-  
+
   if ohai['platform_family'] == 'debian'
     command "curl -O http://dd-agent.s3.amazonaws.com/python-rrdtool/deb/#{ohai['kernel']['machine']}/rrdtool.so", :cwd => "#{install_dir}/embedded/lib/python2.7/"
   elsif ohai['platform_family'] == 'rhel'
@@ -15,15 +15,15 @@ build do
      command "curl -O http://cpansearch.perl.org/src/JHI/perl-5.8.0/thrdvar.h", :cwd => "/usr/lib/perl5/CORE/"
    end
    command "make", :cwd => "/tmp/ExtUtils-MakeMaker-6.31"
-   command "make install", :cwd => "/tmp/ExtUtils-MakeMaker-6.31"
+   command "sudo make install", :cwd => "/tmp/ExtUtils-MakeMaker-6.31"
    command "curl -O http://oss.oetiker.ch/rrdtool/pub/rrdtool-#{version}.tar.gz", :cwd => "/opt/"
    command "tar -xzvf rrdtool-#{version}.tar.gz", :cwd => "/opt/"
    command "./configure", :cwd => "/opt/rrdtool-#{version}", :env => {"PKG_CONFIG_PATH" => "/usr/lib/pkgconfig/"}
    command "make", :cwd => "/opt/rrdtool-#{version}", :env => {"PKG_CONFIG_PATH" => "/usr/lib/pkgconfig/"}
-   command "make install", :cwd => "/opt/rrdtool-#{version}", :env => {"PKG_CONFIG_PATH" => "/usr/lib/pkgconfig/"}
+   command "sudo make install", :cwd => "/opt/rrdtool-#{version}", :env => {"PKG_CONFIG_PATH" => "/usr/lib/pkgconfig/"}
    command "#{install_dir}/embedded/bin/python setup.py install", :cwd => "/opt/rrdtool-#{version}/bindings/python/"
 
 
   end
-  
+
 end
