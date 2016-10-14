335fe8ac056023d394491a548cd89b1c6c0b500b
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2016-10-14 16:49:42 -0400

    [grpcio] fix boringssl vs openssl build conflict.

--- config/software/grpcio.rb
+++ config/software/grpcio.rb
@@ -4,7 +4,12 @@ default_version "1.0.0"
 dependency "python"
 dependency "pip"
 
+env =
+      {
+        "CFLAGS" => "-Ithird_party/boringssl/include",
+      }
+
 build do
   ship_license "https://raw.githubusercontent.com/grpc/grpc/master/LICENSE"
-  command "#{install_dir}/embedded/bin/pip install --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{version}"
+  command "#{install_dir}/embedded/bin/pip install --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{version}", :env => env
 end
