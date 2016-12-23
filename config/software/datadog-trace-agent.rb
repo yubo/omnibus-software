a1ec54b205ce8df3f82f3a0ed03956c2b78cc07f
talwai <aaditya.21@gmail.com>
2016-12-23 13:35:11 -0500

    use custom go1.7 download in build

--- config/software/datadog-trace-agent.rb
+++ config/software/datadog-trace-agent.rb
@@ -5,52 +5,20 @@ env = {
   "GOPATH" => "#{Omnibus::Config.cache_dir}/src/#{name}"
 }
 
-if ohai['platform_family'] == 'mac_os_x'
-  gobin = "/usr/local/bin/go"
-elsif ohai['platform'] == 'windows'
-  gobin = 'C:/Go/bin/go'
-else
-  env['GOROOT'] = "/usr/local/go"
-  gobin = "/usr/local/go/bin/go"
-end
 
-def go_build(program, opts={})
-  default_cmd = "go build -a"
-  if ENV["INCREMENTAL_BUILD"] then
-    default_cmd = "go build -i"
-  end
-  opts = {
-    :cmd => default_cmd
-  }.merge(opts)
-
-  dd = 'main'
-  commit = `git rev-parse --short HEAD`.strip
-  branch = `git rev-parse --abbrev-ref HEAD`.strip
-  date = `date`.strip
-  goversion = `go version`.strip
-  agentversion = ENV["TRACE_AGENT_VERSION"] || "0.99.0"
-
-  ldflags = {
-    "#{dd}.BuildDate" => "#{date}",
-    "#{dd}.GitCommit" => "#{commit}",
-    "#{dd}.GitBranch" => "#{branch}",
-    "#{dd}.GoVersion" => "#{goversion}",
-    "#{dd}.Version" => "#{agentversion}",
-  }.map do |k,v|
-    if goversion.include?("1.4")
-      "-X #{k} '#{v}'"
-    else
-      "-X '#{k}=#{v}'"
-    end
-  end.join ' '
-
-  cmd = opts[:cmd]
-  sh "#{cmd} -ldflags \"#{ldflags}\" #{program}"
-end
+gourl = "https://storage.googleapis.com/golang/go1.7.1.linux-amd64.tar.gz"
+goout = "go.tar.gz"
+godir = "/usr/local/go17"
+gobin = "#{godir}/go/bin/go"
 
 build do
    ship_license "https://raw.githubusercontent.com/DataDog/datadog-trace-agent/#{version}/LICENSE"
    ship_license "https://raw.githubusercontent.com/DataDog/datadog-trace-agent/#{version}/THIRD_PARTY_LICENSES.md"
+   # download go1.7
+   command "wget -O #{goout} #{gourl}"
+   command "mkdir -p #{godir}"
+   command "tar zxfv #{goout} -C #{godir}"
+
    # Download datadog-trace-agent without installing
    command "git clone https://github.com/DataDog/datadog-trace-agent.git $GOPATH/src/github.com/DataDog/datadog-trace-agent", :env => env
    # Checkout datadog-trace-agent's build dependencies
@@ -60,7 +28,7 @@ build do
    command "git checkout 7bc8ce51048e2adc11733f90a87b1c02fb7feebe", :env => env, :cwd => "#{Omnibus::Config.cache_dir}/src/datadog-trace-agent/src/github.com/robfig/glock"
 
    # Checkout and build datadog-trace-agent
-   command "rake restore", :env => env, :cwd => "#{Omnibus::Config.cache_dir}/src/datadog-trace-agent/src/github.com/DataDog/datadog-trace-agent"
+   # command "rake restore", :env => env, :cwd => "#{Omnibus::Config.cache_dir}/src/datadog-trace-agent/src/github.com/DataDog/datadog-trace-agent"
    command "git checkout #{version} && git pull", :env => env, :cwd => "#{Omnibus::Config.cache_dir}/src/datadog-trace-agent/src/github.com/DataDog/datadog-trace-agent"
-   command "go build -i -o trace-agent github.com/DataDog/datadog-trace-agent/agent && mv ./trace-agent #{install_dir}/bin/trace-agent", :env => env, :cwd => "#{Omnibus::Config.cache_dir}/src/datadog-trace-agent/src/github.com/DataDog/datadog-trace-agent"
+   command "#{gobin} build -i -o trace-agent github.com/DataDog/datadog-trace-agent/agent && mv ./trace-agent #{install_dir}/bin/trace-agent", :env => env, :cwd => "#{Omnibus::Config.cache_dir}/src/datadog-trace-agent/src/github.com/DataDog/datadog-trace-agent"
 end
