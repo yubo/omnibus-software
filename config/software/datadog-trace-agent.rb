2397ecfc1e806675015821a5d816ae323330a429
talwai <aaditya.21@gmail.com>
2016-12-23 15:31:28 -0500

    use GOROOT for go17

--- config/software/datadog-trace-agent.rb
+++ config/software/datadog-trace-agent.rb
@@ -3,8 +3,8 @@ default_version "master"
 
 env = {
   "GOPATH" => "#{Omnibus::Config.cache_dir}/src/#{name}"
+  "GOROOT" => "/usr/local/go17/go/bin"
 }
-
 
 gourl = "https://storage.googleapis.com/golang/go1.7.1.linux-amd64.tar.gz"
 goout = "go.tar.gz"
@@ -13,7 +13,6 @@ gobin = "#{godir}/go/bin/go"
 
 build do
    ship_license "https://raw.githubusercontent.com/DataDog/datadog-trace-agent/#{version}/LICENSE"
-   ship_license "https://raw.githubusercontent.com/DataDog/datadog-trace-agent/#{version}/THIRD_PARTY_LICENSES.md"
    # download go1.7
    command "curl #{gourl} -o #{goout}"
    command "mkdir -p #{godir}"
@@ -28,7 +27,7 @@ build do
    command "git checkout 7bc8ce51048e2adc11733f90a87b1c02fb7feebe", :env => env, :cwd => "#{Omnibus::Config.cache_dir}/src/datadog-trace-agent/src/github.com/robfig/glock"
 
    # Checkout and build datadog-trace-agent
-   # command "rake restore", :env => env, :cwd => "#{Omnibus::Config.cache_dir}/src/datadog-trace-agent/src/github.com/DataDog/datadog-trace-agent"
-   command "git checkout #{version} && git pull", :env => env, :cwd => "#{Omnibus::Config.cache_dir}/src/datadog-trace-agent/src/github.com/DataDog/datadog-trace-agent"
+   command "rake restore", :env => env, :cwd => "#{Omnibus::Config.cache_dir}/src/datadog-trace-agent/src/github.com/DataDog/datadog-trace-agent"
+   command "git checkout master && git pull", :env => env, :cwd => "#{Omnibus::Config.cache_dir}/src/datadog-trace-agent/src/github.com/DataDog/datadog-trace-agent"
    command "#{gobin} build -i -o trace-agent github.com/DataDog/datadog-trace-agent/agent && mv ./trace-agent #{install_dir}/bin/trace-agent", :env => env, :cwd => "#{Omnibus::Config.cache_dir}/src/datadog-trace-agent/src/github.com/DataDog/datadog-trace-agent"
 end
