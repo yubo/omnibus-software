f9806293b951dbf38af56f7902bb19a47c95012d
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2017-02-24 15:24:35 -0500

    [jmxfetch] pull from s3 bucket.

--- config/software/jmxfetch.rb
+++ config/software/jmxfetch.rb
@@ -1,33 +1,18 @@
 name 'jmxfetch'
 
-dependency "maven"
-
-env = {
-  "JAVA_HOME" => "#{install_dir}/embedded/jdk",
-}
-
-local_jmx_repo = ENV['LOCAL_JMX_REPO']
-if local_jmx_repo.nil? || local_jmx_repo.empty?
-  source git: 'https://github.com/DataDog/jmxfetch.git'
-else
-  # For local development
-  source path: ENV['LOCAL_JMX_REPO']
-end
-
-jmx_branch = ENV['JMX_BRANCH']
+jmx_branch = ENV['JMX_VERSION']
 if jmx_branch.nil? || jmx_branch.empty?
-  default_version 'master'
+  default_version 'latest'
 else
   default_version jmx_branch
 end
+
+source :url => "https://yumtesting.datad0g.com/testremi/jmxfetch-#{version}-jar-with-dependencies.jar"
 
 relative_path 'jmxfetch'
 
 build do
   ship_license 'https://raw.githubusercontent.com/DataDog/jmxfetch/master/LICENSE'
-
-  command "#{install_dir}/embedded/maven/bin/mvn clean compile assembly:single", :env => env
   mkdir "#{install_dir}/agent/checks/libs"
-  copy 'target/jmxfetch-*-jar-with-dependencies.jar', "#{install_dir}/agent/checks/libs"
-
+  copy 'jmxfetch-*-jar-with-dependencies.jar', "#{install_dir}/agent/checks/libs"
 end
