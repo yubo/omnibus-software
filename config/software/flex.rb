786e5b8bbea5c67d92bd06d8db3256404d1def6a
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2017-07-05 11:22:40 +0200

    [flex] bump to 2.6.4, grab from github.

--- config/software/flex.rb
+++ config/software/flex.rb
@@ -3,8 +3,17 @@ default_version "2.6.0"
 
 relative_path "flex-#{version}"
 
-source :url => "https://downloads.sourceforge.net/project/flex/flex-#{version}.tar.gz",
-       :md5 => "5724bcffed4ebe39e9b55a9be80859ec"
+version "2.6.0" do
+  source :url => "https://downloads.sourceforge.net/project/flex/flex-2.6.0.tar.gz",
+         :md5 => "5724bcffed4ebe39e9b55a9be80859ec"
+end
+
+version "2.6.4" do
+  source :url => "https://github.com/westes/flex/releases/download/v2.6.4/flex-2.6.4.tar.gz",
+         :sha256 => "e87aae032bf07c26f85ac0ed3250998c37621d95f8bd748b31f15b33c45ee995"
+end
+
+default_version "2.6.4"
 
 env = with_standard_compiler_flags()
 
