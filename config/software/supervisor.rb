3a04c174cc9d3678982da5bb09ebf1e633b5cf6d
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2017-09-18 14:54:51 +0200

    [supervisor] trying with create the links

--- config/software/supervisor.rb
+++ config/software/supervisor.rb
@@ -4,18 +4,18 @@ default_version "3.3.3"
 dependency "python"
 dependency "pip"
 
-supervisord_file = "#{install_dir}/bin/supervisord"
-supervisorctl_file = "#{install_dir}/bin/supervisorctl"
-pidproxy_file = "#{install_dir}/bin/pidproxy"
-
 build do
+  supervisord_file = "#{install_dir}/bin/supervisord"
+  supervisorctl_file = "#{install_dir}/bin/supervisorctl"
+  pidproxy_file = "#{install_dir}/bin/pidproxy"
+
   ship_license "https://raw.githubusercontent.com/Supervisor/supervisor/master/LICENSES.txt"
   pip "install --install-option=\"--install-scripts=#{install_dir}/bin\" #{name}==#{version}"
 
   # install-option not being honored with 3.3.3
   if !File.exists?(supervisord_file)
-    link "#{install_dir}/embedded/bin/supervisord", "#{supervisord_file}"
-    link "#{install_dir}/embedded/bin/supervisorctl", "#{supervisorctl_file}"
-    link "#{install_dir}/embedded/bin/pidproxy", "#{pidproxy_file}"
+    link "#{install_dir}/embedded/bin/supervisord", "#{supervisord_file}", :force
+    link "#{install_dir}/embedded/bin/supervisorctl", "#{supervisorctl_file}", :force
+    link "#{install_dir}/embedded/bin/pidproxy", "#{pidproxy_file}", :force
   end
 end
