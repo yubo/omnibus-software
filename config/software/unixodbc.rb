324efa1b4d8a5edde434ca23da7631e9a114bfbc
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2018-01-17 18:57:56 -0500

    [unixodbc] automake, autoconf and libtool likely already in builders

--- config/software/unixodbc.rb
+++ config/software/unixodbc.rb
@@ -1,10 +1,6 @@
 name "unixodbc"
 default_version "2.3.4"
 
-dependency "automake"
-dependency "autoconf"
-dependency "libtool"
-
 version "2.3.4" do
   source :sha256 => "2e1509a96bb18d248bf08ead0d74804957304ff7c6f8b2e5965309c632421e39"
 end
