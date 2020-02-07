69a4f69783204e1239c2776c57e0edd3a71283b2
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2020-02-07 04:44:26 -0500

    [nghttp2] glibc versioning too

--- config/software/nghttp2.rb
+++ config/software/nghttp2.rb
@@ -8,10 +8,14 @@ source url: "https://github.com/nghttp2/nghttp2/releases/download/v1.12.0/nghttp
 
 relative_path "nghttp2-#{version}"
 
-env = {
-  "OPENSSL_CFLAGS" => "-I#{install_dir}/embedded/include/openssl",
-  "OPENSSL_LIBS" => "-L#{install_dir}/embedded/lib",
-}
+env = with_standard_compiler_flags(with_embedded_path)
+env["OPENSSL_CFLAGS"] = "-I#{install_dir}/embedded/include/openssl"
+env["OPENSSL_LIBS"] = "-L#{install_dir}/embedded/lib"
+env["LD_RUN_PATH"] = "#{install_dir}/embedded/lib"
+
+if linux?
+  env = with_glibc_version(env)
+end
 
 build do
   command [
@@ -21,6 +25,6 @@ build do
     "--disable-hpack-tools",
     "--prefix=#{install_dir}/embedded",
   ].join(" "), env: env
-  command "make -j #{workers}", env: { "LD_RUN_PATH" => "#{install_dir}/embedded/lib" }
+  command "make -j #{workers}", env: {  }
   command "make install"
 end
