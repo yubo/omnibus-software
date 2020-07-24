f047a1b9072435228df39f60186b5948f89f0770
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2020-07-24 16:04:11 -0400

    [deps] bump pyyaml to 5.3.1 + pip to 20.1.1 (#370)
    * [pyyaml] bump to latest 5.3.1
    
    * [pip] bump to 20.1.1
    
    * [pip] rubocop: extra empty line
    
    * [pyyaml] Update license
    
    * [pyyaml] Apply regex-at-runtime patch
    
    Backport of
    https://github.com/DataDog/datadog-agent/blob/master/omnibus/config/patches/datadog-agent-integrations-py2/create-regex-at-runtime.patch
    
    Avoids memory usage increase of pyyaml v5 on python 2.
    
    Co-authored-by: Olivier Vielpeau <olivier.vielpeau@datadoghq.com>

--- config/software/pyyaml.rb
+++ config/software/pyyaml.rb
@@ -1,5 +1,5 @@
 name "pyyaml"
-default_version "3.11"
+default_version "5.3.1"
 
 dependency "python"
 dependency "pip"
@@ -11,8 +11,15 @@ else
 end
 
 build do
-  ship_license "http://dd-agent-omnibus.s3.amazonaws.com/pyyaml-LICENSE"
+  ship_license "https://raw.githubusercontent.com/yaml/pyyaml/#{version}/LICENSE"
   pip "install --install-option=\"--install-scripts="\
       "#{windows_safe_path(install_dir)}/bin\" "\
       "#{name}==#{version}"
+
+  # Patch applies to only one file: set it explicitly as a target, no need for -p
+  if windows?
+    patch :source => "create-regex-at-runtime.patch", :target => "#{install_dir}/embedded/Lib/site-packages/yaml/reader.py"
+  else
+    patch :source => "create-regex-at-runtime.patch", :target => "#{install_dir}/embedded/lib/python2.7/site-packages/yaml/reader.py"
+  end
 end
